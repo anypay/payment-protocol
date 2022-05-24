631a504d83d6e4c4dab1d5772ca626b26c9165eb
Steven Zeiler <me@stevezeiler.com>
2022-05-24 02:12:20 +0200

    Typescript Client for Payment Protocol

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+
+export { TestClient } from './test_client'
+
---
+++ src/test_client.ts
@@ -0,0 +1,111 @@
+
+import axios from 'axios'
+
+import * as protocol from './protocol'
+
+interface TestClientOptions {
+  headers?: any;
+}
+
+export class TestClient {
+
+  url: string;
+
+  supertest: any;
+
+  headers: any;
+
+  constructor(supertest: any, url: string, options: TestClientOptions = {}) {
+
+    this.supertest = supertest
+
+    this.url = url
+
+    console.log("options.headers", options.headers)
+
+    this.headers = options.headers || {}
+
+  }
+
+  async getPaymentOptions(): Promise<protocol.PaymentOptions> {
+
+    const headers = Object.assign(this.headers, {
+      'x-paypro-version': 2,
+      'accept': 'application/payment-options'
+    })
+
+    console.log('DA_HEAD', headers)
+
+    let { result } = await this.supertest.inject({
+      method: 'GET',
+      url: this.url,
+      headers
+    })
+
+    return result
+
+  }
+
+  async selectPaymentOption(params: protocol.SelectPaymentRequest): Promise<protocol.PaymentRequest> {
+
+    let { result } = await this.supertest.inject({
+      method: "POST",
+      url: this.url,
+      payload: params,
+      headers: Object.assign(this.headers, {
+        'x-paypro-version': 2,
+        'content-type': 'application/payment-request'
+      })
+    })
+
+    return result
+
+  }
+
+  async verifyPayment(params: protocol.PaymentVerificationRequest): Promise<protocol.PaymentVerification> {
+
+    let { result } = await this.supertest.inject({
+      method: 'POST',
+      url: this.url, 
+      payload: params,
+      headers: Object.assign(this.headers, {
+        'x-paypro-version': 2,
+        'content-type': 'application/payment-verification'
+      })
+    })
+
+    return result
+
+  }
+
+  async sendPayment(
+
+    params: protocol.Payment
+
+  ): Promise<protocol.PaymentResponse> {
+
+    const payment: protocol.SendPayment = {
+
+      chain: params.chain,
+
+      currency: params.chain,
+
+      transactions: params.transactions
+    }
+
+    let { result } = await this.supertest.inject({
+      method: "POST",
+      url: this.url,
+      payload: payment,
+      headers: Object.assign(this.headers, {
+        'x-paypro-version': 2,
+        'content-type': 'application/payment'
+      })
+    })
+
+    return result
+
+  }
+
+}
+
